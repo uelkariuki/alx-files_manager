352c2ce0808dad9dfe40c67c9fcab7735bbc00e1
abraham-ship <abuzsein3@gmail.com>
2024-02-12 20:16:36 +0000

    fix eslint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.listen(port, () => {
-	  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 app.use('/', indexRouter);
 
