c40f0c9132cee4bab3a0e00974e45be7665f1cb7
abraham-ship <abuzsein3@gmail.com>
2024-02-13 17:05:34 +0000

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-
+import AuthController from '../controllers/AuthController';
 const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
@@ -9,5 +9,8 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
