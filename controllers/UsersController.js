07ccbd5d80b2ddfa873be30c570da7d83cef0e12
uelkariuki <cratzuel@gmail.com>
2024-02-15 05:42:32 +0300

    Fix api errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,7 @@
 import redisClient from '../utils/redis';
 
 const crypto = require('crypto');
-const client = require('../utils/db');
+const dbClient = require('../utils/db');
 
 exports.postNew = async (req, res) => {
   const { email, password } = req.body;
@@ -21,14 +21,14 @@ exports.postNew = async (req, res) => {
 
   try {
     // check if user with an email already exists
-    const userExists = await client.dbClient.db
+    const userExists = await dbClient.db
       .collection('users')
       .findOne({ email });
     if (userExists) {
       return res.status(400).send({ error: 'Already exist' });
     }
     // if user does not exist insert a new user with hashed password
-    const result = await client.dbClient.db
+    const result = await dbClient.db
       .collection('users')
       .insertOne({ email, password: hashedPassword });
     const user = result.ops[0];
@@ -50,7 +50,7 @@ exports.getMe = async (request, response) => {
   const redisToken = await redisClient.get(`auth_${token}`);
   if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
 
-  const user = await client.dbClient.users.find(`ObjectId("${userId}")`).toArray();
+  const user = await dbClient.users.find(`ObjectId("${userId}")`).toArray();
   if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
   const processedUser = { id: user._id, ...user };
